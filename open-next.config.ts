a61826aa31268a51364ca328fdcc6e0b3b0ff5fa
NapiBaseCEOs <businessxtro@gmail.com>
2025-11-16 17:57:52 +0300

    OpenNext config external property kaldirildi - TypeScript hatasi cozuldu

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,10 +1,4 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
-  // Turso client için external dependencies
-  external: [
-    "@libsql/isomorphic-ws",
-    "@libsql/client/web",
-  ],
-});
+export default defineCloudflareConfig();
 
